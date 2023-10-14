e7616bd44d0e0e600d63b6c53433f65d5e7c368d
Mikhail Kravchenko <km.code@proton.me>
2023-10-14 16:21:48 +0300

    First commit, groundwork for django

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.get_routes, name="routes"),
     path('notes/', views.get_notes, name="notes"),
     path('notes/<str:pk>/', views.get_note, name="note"),
+#     added comment
 ]
